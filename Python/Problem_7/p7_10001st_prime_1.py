1ce9277e7f4aa821a4a89608b785ce1132b4a9cf
Kevin Huo <khbhuo@uwaterloo.ca>
2016-01-25 10:36:27 -0500

    added changes to p7 solution

--- Python/Problem_7/p7_10001st_prime_1.py
+++ Python/Problem_7/p7_10001st_prime_1.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
-def primes(up_to, factorslist):
-	while i <= 10001:
-		factorslist = filter(lambda x: x == i or x % i, factorslist)
-		yield factorslist
+def primes(up_to):
+	n = 20000
+	i = 
+	factorslist = range(2, n)
+	while i <= up_to:
+		list1 = filter(lambda x: x == i or x % i, factorslist)
+		i = len(list1)
+		n += 2
+	return list1
+
+list2 = primes(10001)
+
+print list2[-1]
